89d0eb866a4ee0df82598864ff14a458ab48e98c
omespef1 <omespef1@hotmail.com>
2019-10-06 01:26:47 -0500

    v
    v

---
+++ src/app/models/enlistmen/manchecklist.ts
@@ -0,0 +1,14 @@
+export class manchecklist {
+  IdEmpresa: number;
+  Id: number;
+  IdVehiculo: number;
+  IdTercero: number;
+  FechaProceso: Date;
+  Reviso: string;
+  Estado: string;
+  Observaciones: string;
+  Acepto: string;
+  NumeroViaje: string;
+  Kilometraje: number;
+  CentroId: number;
+}
