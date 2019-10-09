8cdc835ac3b4951c624f09a792d02ec38b8ab71a
omespef1 <omespef1@hotmail.com>
2019-10-09 17:56:32 -0500

    v
    v

--- src/app/pages/enlistment/enlistment.page.ts
+++ src/app/pages/enlistment/enlistment.page.ts
@@ -121,7 +121,7 @@ export class EnlistmentPage implements OnInit {
     this.snapshot = true;
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     };
