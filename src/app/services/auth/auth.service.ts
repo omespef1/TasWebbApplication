acf8697881a79261a973ea8dde0343bab7860aec
omespef1 <omespef1@hotmail.com>
2020-10-22 01:59:30 -0500

    v
    v

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -142,10 +142,10 @@ export class AuthService {
     this._sesion.getOneSignalId().then((resp) => {
       if (resp !== undefined && resp !== null) {
         const userOneSingnal: OneSignalEntitie = new OneSignalEntitie();
-        userOneSingnal.CompanyId = this._sesion.GetThirdPartie().IdEmpresa;
         userOneSingnal.OneSignalId = resp.userId;
         if (this._sesion.isUser()) {
           userOneSingnal.UserName = this._sesion.GetUser().NombreCompleto;
+          userOneSingnal.CompanyId = this._sesion.GetUser().IdEmpresa;
           this.usersOneSignal.PostOneSignalUser(userOneSingnal).subscribe((resp:transaction)=>{
             if(resp.Retorno==0){
               console.log('NOT OK');
@@ -153,6 +153,7 @@ export class AuthService {
           })
         } else {
           userOneSingnal.ThirdPartie = this._sesion.GetThirdPartie().IdTercero;
+          userOneSingnal.CompanyId = this._sesion.GetThirdPartie().IdEmpresa;
           this._thirdPartieOneSignal.PostOneSignalThirdPartie(userOneSingnal).subscribe((resp:transaction)=>{
             if(resp.Retorno==0){
               console.log('NOT OK');
