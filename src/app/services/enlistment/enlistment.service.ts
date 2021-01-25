fcf85a0e8f298f966e3d73c0439fa81259d26247
omespef1 <omespef1@hotmail.com>
2021-01-24 22:00:25 -0500

    Update enlistment.service.ts
    V

--- src/app/services/enlistment/enlistment.service.ts
+++ src/app/services/enlistment/enlistment.service.ts
@@ -26,17 +26,17 @@ export class EnlistmentService {
   }
 
   PostAnswer(answers: manchecklist) {
-    // return this._http.Post<    background-color: #F2F2F2;>("/vehicle", answers);
-    const headerDict = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Origin": "*"
-    };
-    let bodyRequest: any = {
-      headers: new HttpHeaders(headerDict)
-    };
-    return this.http.post<transactionID>(`${config.url} /vehicle`, answers, bodyRequest as object).pipe(retry(5));
+    return this._http.PostRequest<transactionID>("/vehicle", answers);
+    // const headerDict = {
+    //   "Content-Type": "application/json",
+    //   Accept: "application/json",
+    //   "Access-Control-Allow-Headers": "Content-Type",
+    //   "Access-Control-Allow-Origin": "*"
+    // };
+    // let bodyRequest: any = {
+    //   headers: new HttpHeaders(headerDict)
+    // };
+    // return this.http.post<transactionID>(`${config.url} /vehicle`, answers, bodyRequest as object).pipe(retry(5));
   }
 
   CheckEnlistment(enlistment: manchecklist) {
