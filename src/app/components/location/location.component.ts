328c4fe064e4df371430e9dbbdbd76fb57c6b5f9
omespef1 <omespef1@hotmail.com>
2023-10-28 00:28:17 -0500

    ajustes waze

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -27,15 +27,20 @@ export class LocationComponent implements OnInit {
     
   }
 
-  openLocation() {
+  openLocation(app:string) {
     // Reemplaza las comas si existen
     let lat = this.detail.Latitude.replace(',', '.');
     let long = this.detail.Longitude.replace(',', '.');
     
     // Construye la URL para Google Maps como ejemplo. 
     // La mayoría de las veces, esto desencadenará el comportamiento del selector de aplicaciones si hay múltiples apps que pueden manejar la URL.
-    const url = `https://www.google.com/maps/search/?api=1&query=${lat},${long}`;
-    
+   
+    let url:string="";
+    if(app=='waze')
+    url= `https://waze.com/ul?ll=${lat},${long}&navigate=yes`;
+    if(app=='maps')
+    url = `https://www.google.com/maps/search/?api=1&query=${lat},${long}`;
+  
     // Abre la URL
     // window.open(url, '_system');
     this.browserTab.create(url, '_system', 'location=yes');
--- src/assets/config/settings.ts
+++ src/assets/config/settings.ts
@@ -1,3 +1,3 @@
 export class config {
-  public static readonly  url = 'http://localhost/RTASWEB/api';
+  public static readonly  url = 'http://192.168.18.208/RTASWEB/api';
 }
